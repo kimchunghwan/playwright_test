f190d2b56978309fe5c2ce1ebee93d001de05a50
kim chunghwan <keii2k84@gmail.com>
2024-01-14 21:48:38 +0900

    first

---
+++ tests/test-2.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+const testResultPath = 'test-results';
+test('test', async ({ page }) => {
+  await page.goto('https://finance.naver.com/sise/sise_index.naver?code=KOSPI');
+  await page.getByText('월', { exact: true }).click();
+  await page.locator('.ciq-chart-area').screenshot({ path: `./${testResultPath}/KOSPI.png` })
+
+  await page.goto('https://finance.naver.com/sise/sise_index.naver?code=KOSDAQ');
+  await page.getByText('월', { exact: true }).click();
+  await page.locator('.ciq-chart-area').screenshot({ path: `./${testResultPath}/KOSDAQ.png` })
+});
