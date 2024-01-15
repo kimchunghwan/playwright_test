bfb086ee344c3d8cc8533ef5b5269aad659043fd
kim chunghwan <keii2k84@gmail.com>
2024-01-15 10:46:54 +0900

    upload to slack

--- tests/test-2.spec.ts
+++ tests/test-2.spec.ts
@@ -3,9 +3,9 @@ const testResultPath = 'test-results';
 test('test', async ({ page }) => {
   await page.goto('https://finance.naver.com/sise/sise_index.naver?code=KOSPI');
   await page.getByText('월', { exact: true }).click();
-  await page.locator('.ciq-chart-area').screenshot({ path: `./${testResultPath}/KOSPI.png` })
+  await page.locator('.ciq-chart-area').screenshot({ path: `./${testResultPath}/KOSPI_monthly.png` })
 
   await page.goto('https://finance.naver.com/sise/sise_index.naver?code=KOSDAQ');
   await page.getByText('월', { exact: true }).click();
-  await page.locator('.ciq-chart-area').screenshot({ path: `./${testResultPath}/KOSDAQ.png` })
+  await page.locator('.ciq-chart-area').screenshot({ path: `./${testResultPath}/KOSDAQ_monthly.png` })
 });
