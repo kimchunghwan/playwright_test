37b0fdfe602f9bcb3aabb8b5195a1bff9d279471
kim chunghwan <keii2k84@gmail.com>
2024-01-15 11:30:49 +0900

    finvize

--- tests/test-2.spec.ts
+++ tests/test-2.spec.ts
@@ -13,11 +13,12 @@ test('test', async ({ page }) => {
 });
 
 test('finviz home', async({page}) => {
+  page.setViewportSize({ width: 1920, height: 1920 })
   try {
     page.goto('https://finviz.com/'); 
   } catch (error) {
     // finviz쪽에서 크롤링을 막은듯해서 state load가 안되는듯 하여 임시처리
   }
   await page.waitForTimeout(10000);
-  await page.locator('#homepage').screenshot({ path: `./${testResultPath}/fear-and-greed.png` })
+  await page.locator('.fv-container').screenshot({ path: `./${testResultPath}/fear-and-greed.png` })
 })
