6d5d61470051f398444df97b3148b9ecfcf1d15a
kim chunghwan <keii2k84@gmail.com>
2024-01-15 10:57:32 +0900

    edit

--- tests/test-2.spec.ts
+++ tests/test-2.spec.ts
@@ -3,9 +3,11 @@ const testResultPath = 'test-results';
 test('test', async ({ page }) => {
   await page.goto('https://finance.naver.com/sise/sise_index.naver?code=KOSPI');
   await page.getByText('월', { exact: true }).click();
+  await page.waitForTimeout(1000);
   await page.locator('.ciq-chart-area').screenshot({ path: `./${testResultPath}/KOSPI_monthly.png` })
-
+  
   await page.goto('https://finance.naver.com/sise/sise_index.naver?code=KOSDAQ');
   await page.getByText('월', { exact: true }).click();
+  await page.waitForTimeout(1000);
   await page.locator('.ciq-chart-area').screenshot({ path: `./${testResultPath}/KOSDAQ_monthly.png` })
 });
