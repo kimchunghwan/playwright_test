abe904e31d4c509749ec076fc6e130e39c0a4b98
kim chunghwan <keii2k84@gmail.com>
2024-01-15 11:23:32 +0900

    finviz home

--- tests/test-2.spec.ts
+++ tests/test-2.spec.ts
@@ -11,3 +11,13 @@ test('test', async ({ page }) => {
   await page.waitForTimeout(1000);
   await page.locator('.ciq-chart-area').screenshot({ path: `./${testResultPath}/KOSDAQ_monthly.png` })
 });
+
+test('finviz home', async({page}) => {
+  try {
+    page.goto('https://finviz.com/'); 
+  } catch (error) {
+    // finviz쪽에서 크롤링을 막은듯해서 state load가 안되는듯 하여 임시처리
+  }
+  await page.waitForTimeout(10000);
+  await page.locator('#homepage').screenshot({ path: `./${testResultPath}/fear-and-greed.png` })
+})
