92f5905ea68bb647d70665713495cf4b809f878c
kim chunghwan <keii2k84@gmail.com>
2024-01-23 10:44:40 +0900

    edit

--- tests/test-2.spec.ts
+++ tests/test-2.spec.ts
@@ -16,11 +16,10 @@ test('finviz home', async({page}) => {
   page.setViewportSize({ width: 1920, height: 1920 })
   try {
     page.goto('https://finviz.com/');
+    await page.waitForTimeout(10000);
+    page.locator('.fv-container').screenshot({ path: `./${testResultPath}/finviz-home.png` })
+    await page.waitForTimeout(5000);
   } catch (error) {
     // finviz쪽에서 크롤링을 막은듯해서 state load가 안되는듯 하여 임시처리
   }
-  await page.waitForTimeout(10000);
-  page.locator('.fv-container').screenshot({ path: `./${testResultPath}/finviz-home.png` })
-  await page.waitForTimeout(5000);
-
 })
