08ff0ab45eb70d67fa96b3a21d9b7b7b366bbd79
kim chunghwan <keii2k84@gmail.com>
2024-01-23 10:43:44 +0900

    edit

--- tests/test-2.spec.ts
+++ tests/test-2.spec.ts
@@ -5,7 +5,7 @@ test('test', async ({ page }) => {
   await page.getByText('월', { exact: true }).click();
   await page.waitForTimeout(1000);
   await page.locator('.ciq-chart-area').screenshot({ path: `./${testResultPath}/KOSPI_monthly.png` })
-  
+
   await page.goto('https://finance.naver.com/sise/sise_index.naver?code=KOSDAQ');
   await page.getByText('월', { exact: true }).click();
   await page.waitForTimeout(1000);
@@ -15,11 +15,12 @@ test('test', async ({ page }) => {
 test('finviz home', async({page}) => {
   page.setViewportSize({ width: 1920, height: 1920 })
   try {
-    page.goto('https://finviz.com/'); 
+    page.goto('https://finviz.com/');
   } catch (error) {
     // finviz쪽에서 크롤링을 막은듯해서 state load가 안되는듯 하여 임시처리
   }
   await page.waitForTimeout(10000);
   page.locator('.fv-container').screenshot({ path: `./${testResultPath}/finviz-home.png` })
+  await page.waitForTimeout(5000);
 
 })
