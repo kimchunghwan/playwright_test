5b624fffd49e6f4d9551e17da2c89c625ce1c9f8
kim chunghwan <keii2k84@gmail.com>
2024-02-17 10:52:07 +0900

    add symbols

--- tests/test-2.spec.ts
+++ tests/test-2.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from "@playwright/test";
+
 const testResultPath = "test-results";
 test("test", async ({ page }) => {
   await page.goto("https://finance.naver.com/sise/sise_index.naver?code=KOSPI");
@@ -18,7 +19,12 @@ test("test", async ({ page }) => {
     .screenshot({ path: `./${testResultPath}/KOSDAQ_monthly.png` });
 });
 
-const finvizCapture = async (page: any, url: string, path: string, locator:string) => {
+const finvizCapture = async (
+  page: any,
+  url: string,
+  path: string,
+  locator: string
+) => {
   page.setViewportSize({ width: 1920, height: 1920 });
   try {
     page.goto(url).catch((e) => {});
@@ -40,30 +46,20 @@ test("finviz home", async ({ page }) => {
     `./${testResultPath}/finviz-home.png`,
     ".fv-container"
   );
-  // page.setViewportSize({ width: 1920, height: 1920 })
-  // try {
-  //   page.goto('https://finviz.com/').catch((e) => {});
-  //   await page.waitForTimeout(10000);
-  //   page.locator('.fv-container').screenshot({ path: `./${testResultPath}/finviz-home.png` }).catch((e) => {});
-  //   await page.waitForTimeout(5000);
-  // } catch (error) {
-  //   // finviz쪽에서 크롤링을 막은듯해서 state load가 안되는듯 하여 임시처리
-  // }
 });
-test("finviz tesla", async ({ page }) => {
-  await finvizCapture(
-    page,
-    "https://finviz.com/quote.ashx?t=TSLA&p=d",
-    `./${testResultPath}/finviz-tesla.png`,
-    "#chart"
-  );
-  // page.setViewportSize({ width: 1920, height: 1920 })
-  // try {
-  //   page.goto('https://finviz.com/').catch((e) => {});
-  //   await page.waitForTimeout(10000);
-  //   page.locator('.fv-container').screenshot({ path: `./${testResultPath}/finviz-home.png` }).catch((e) => {});
-  //   await page.waitForTimeout(5000);
-  // } catch (error) {
-  //   // finviz쪽에서 크롤링을 막은듯해서 state load가 안되는듯 하여 임시처리
-  // }
+
+test.describe("finviz sectors", () => {});
+
+test.describe("finviz symbols", () => {
+  const symbols = ["TSLA", "PLTR", "ADBE", "SNOW", "QQQ", "SOXX"];
+  for (const symbol of symbols) {
+    test(`finviz ${symbol}`, async ({ page }) => {
+      await finvizCapture(
+        page,
+        `https://finviz.com/quote.ashx?t=${symbol}&p=d`,
+        `./${testResultPath}/finviz-${symbol}.png`,
+        "#chart"
+      );
+    });
+  }
 });
